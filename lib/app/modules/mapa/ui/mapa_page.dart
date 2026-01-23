044e8e84c6d5f8bf18c2c5f670eb34f372db4b18
Guilherme Martins <glhmartinsrangel@gmail.com>
2026-01-23 08:10:51 -0300

    popup ao clicar no marker

--- lib/app/modules/mapa/ui/mapa_page.dart
+++ lib/app/modules/mapa/ui/mapa_page.dart
@@ -12,46 +12,58 @@ class MapaPage extends GetView<MapaController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Appbar(title: "Mapa"),
-
-      body: Mapa(),
+      appBar: Appbar(title: "AutoMonitor"),
+      body: mapa(),
     );
   }
 
-  Widget Mapa() {
+  Widget mapa() {
     return FlutterMap(
       options: MapOptions(
         initialCenter: LatLng(-22.8966, -43.1238),
         initialZoom: 15.0,
       ),
-      children: [openstreetmap, Markers()],
+      children: [openstreetmap, markers()],
     );
   }
 
-  TileLayer get openstreetmap => TileLayer(
+  TileLayer get openstreetmap => TileLayer( // Renderiza o mapa
     urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
     userAgentPackageName: 'com.example.automonitor',
   );
 
-  Widget Markers() {
-    return Obx(() {
+  Widget markers() { // Coloca os marcadores no mapa
+    return Obx(() { // Atualiza quando algum marcador se move
       return MarkerLayer(
         markers: controller.veiculos.map((veiculo) {
           return Marker(
             point: LatLng(veiculo.lat, veiculo.long),
             child: GestureDetector(
-              onTap: () {
-                // popup depois
-              },
+              onTap: () {Get.dialog(popUp(veiculo));},
               child: Icon(
                 Icons.location_pin,
                 color: Colors.red,
-                size: 40,
+                size: 50,
               ),
             ),
           );
         }).toList(),
       );
     });
+  }
+
+  Widget popUp(Veiculo veiculo) {
+    return AlertDialog(
+      title: Text("Veiculo"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text('ID:  ${veiculo.id}'),
+          Text('Lat: ${veiculo.lat}'),
+          Text('Lng: ${veiculo.long}'),
+        ],
+      ),
+    );
   }
 }
